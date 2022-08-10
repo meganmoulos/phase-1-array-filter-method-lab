13c29bb906fb2222d5f9681d14d5f770a7fddd9f
Megan <meganmoulos@gmail.com>
2022-08-10 14:46:02 -0700

    commit tree a9a8985c48f3d3a9c1a77b24cfb3db3df01136e6

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+    // return matching list of drivers
+    // case insensitive 
+    // use filter()
+
+function findMatching(drivers, string) {
+    return drivers.filter(word => word.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(word => word[0] === string[0])
+}
+
+function matchName(drivers, string) {
+    let driver = drivers[0].name;
+    return drivers.filter(word => word.name === string);
+}
