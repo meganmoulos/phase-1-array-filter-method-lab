52ba2b8720d16d3427909933221bbde667226a95
Megan <meganmoulos@gmail.com>
2022-08-10 14:46:29 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(word => word.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(word => word[0] === string[0])
+}
+
+function matchName(drivers, string) {
+    let driver = drivers[0].name;
+    return drivers.filter(word => word.name === string);
+}
